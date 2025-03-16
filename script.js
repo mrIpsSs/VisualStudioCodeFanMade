823c1bc24d7f3795c64db0123625bc18522023dc
mrIpsSs <thebestbsnx@gmail.com>
2025-03-17 00:55:07 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,13 @@
+window.addEventListener("DOMContentLoaded", function () {
+    VANTA.NET({
+        el: "#vanta-bg",
+        mouseControls: true,
+        touchControls: true,
+        gyroControls: true,
+        color: 0xffffff,
+        backgroundColor: 0x000000,
+        points: 7.00,
+        maxDistance: 30.00,
+        spacing: 25.00
+    });
+});
